f19a652cb60105ab66708284c543d8a2020ee60e
maevanapcontact <maevanap.contact@gmail.com>
2018-07-04 10:03:17 +0200

    feat: display 5 markers

--- src/App.js
+++ src/App.js
@@ -16,24 +16,30 @@ class App extends Component {
   }
 
   initMap = () => {
+    const { locations } = this.state;
+
+    /* Define the map */
     let map = new window.google.maps.Map(document.getElementById('map'), {
       zoom: 13,
       center: { lat: 60.1699, lng: 24.9384 }
     });
 
-    let marker = new window.google.maps.Marker({
-      position: { lat: 60.1699, lng: 24.9384 },
-      map: map,
-      title: 'Helsinki Cnter'
-    });
+    /* Create a marker for each location in the locations.json file */
+    for (let i = 0; i < locations.length; i++) {
+      /* Define the values of the properties */
+      let position = locations[i].position;
+      let title = locations[i].title;
+      let id = locations[i].key
 
-    let infoWindow = new window.google.maps.InfoWindow({
-      content: 'display content'
-    });
-
-    marker.addListener('click', function () {
-      infoWindow.open(map, marker);
-    });
+      /* Create the marker itself */
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: id
+      });
+    }
   }
 
   render() {
